21f8283692a7323a135b63ef6c3d2ec736811005
Alexey Makarov <knight17@mail.ru>
2018-04-16 23:27:31 +0300

    Add files via upload

---
+++ AllCppBaisics-6MainThings/main.h
@@ -0,0 +1,19 @@
+////////////////////////////////
+
+#define RAVNO_I_ZAPATAYA 0
+#define PEREMENNIE		 0
+#define PREOBRAZOV_TIPOV 0
+#define FUNKZII			 0
+#define MATEMAT_OPERAZII 0
+#define LOGI4ES_OPERAZII 0
+#define OPERATORI		 0
+#define NAMESPACE		 0
+
+#define ETRA_EXAMPLE_1	 0
+#define ETRA_EXAMPLE_2	 0
+#define ETRA_EXAMPLE_3	 0
+
+#define EXAMPLE_ALGORITM 0
+#define EXAMPLE_WINAPI	 0
+
+#define ALGORITMI_I_SORT 0
